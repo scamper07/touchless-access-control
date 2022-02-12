f2a8322cf361e3d156cf1d5f02dff8bd8d3ebbf7
Preetham Kulai <preetham.kulai21@gmail.com>
2022-02-12 11:26:41 +0530

    Delete touchless-access-control directory

--- touchless-access-control/test/test_api.py
+++
@@ -1,5 +0,0 @@
-import requests
-for i in range(11,15):
-    res = requests.post('http://localhost:5000/api/addkey', json={"id":i, "value": "qwerty"+str(i), "lockid":2})
-    if res.ok:
-        print(res.json())
